92bbbdf305944120ca13e62c02e537904dcf3b00
Andrey Zimin <zimin@chatlanin.com.ru>
2020-10-02 23:19:08 +0300

    check user passwd

--- 2part.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ansible-playbook settings.yml
--- install/install.sh
+++ install/install.sh
@@ -2,4 +2,4 @@
 mount / -o remount,size=1G /run/archiso/cowspace
 pacman -S ansible python-pip
 pip install pexpect
-ansible-playbook empty.yml
+ansible-playbook main.yml
